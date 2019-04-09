7afecb8036c04f242421d3571b8d617b80338f77
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-04-09 16:45:05 -0300

    Some adjusts

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 const fs = require('fs');
-const http = require('http').createServer(handler);
 const express = require('express');
 const bodyParser = require('body-parser');
 const Redis = require('ioredis');
-const redis = new Redis(6379, 'redis');
-const redis2 = new Redis(6379, 'redis');
+const redisSub = new Redis(6379, 'redis');
+const redisPub = new Redis(6379, 'redis');
 
-redis.psubscribe('*', () => {
-    redis.on('pmessage', (pattern, channel, message) => {
+redisSub.psubscribe('*', () => {
+    redisSub.on('pmessage', (pattern, channel, message) => {
         io.emit(channel, message);
     });
 });
-redis.on("error", function(err){
+redisSub.on("error", function(err){
     console.log(err);
 });
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
@@ -36,36 +35,13 @@ app.get('/', (req, res) => {
         res.writeHead(200);
         res.end(data);
     });
-    io.on('products', (data) => {
-        console.log(data);
-        const el = document.querySelector('#message');
-        el.innerHTML = "<h1>" + data + "</h1>";
-    });
-    // res.send('teste');
 })
 
 app.post('/publish', (req, res) => {
     const body = req.body;
-    redis2.publish('products', body.mensagem).then((resposta) => {
+    redisPub.publish('products', body.mensagem).then((resposta) => {
         console.log(resposta);
     });
     res.status(200);
     res.json(body);
 });
-
-
-// http.listen(3000, () => {
-//     console.log('Listening on Port 3000');
-// });
-
-function handler (req, res) {
-    fs.readFile(__dirname + '/index.html', (err, data) => {
-        if (err) {
-            res.writeHead(500);
-            return res.end('Error loading index.html');
-        }
-
-        res.writeHead(200);
-        res.end(data);
-    });
-}
