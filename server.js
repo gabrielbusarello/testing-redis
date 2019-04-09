41a9552bae653b2cc0d7a0643c888174a19d6172
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-04-09 16:27:22 -0300

    Added express to application for dispatch messages

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const http = require('http').createServer(handler);
-const io = require('socket.io')(http);
+const express = require('express');
+const bodyParser = require('body-parser');
 const Redis = require('ioredis');
 const redis = new Redis(6379, 'redis');
+const redis2 = new Redis(6379, 'redis');
 
 redis.psubscribe('*', () => {
     redis.on('pmessage', (pattern, channel, message) => {
@@ -13,9 +15,48 @@ redis.on("error", function(err){
     console.log(err);
 });
 
-http.listen(3000, () => {
-    console.log('Listening on Port 3000');
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+const server = app.listen(3000);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+
+        res.writeHead(200);
+        res.end(data);
+    });
+    io.on('products', (data) => {
+        console.log(data);
+        const el = document.querySelector('#message');
+        el.innerHTML = "<h1>" + data + "</h1>";
+    });
+    // res.send('teste');
+})
+
+app.post('/publish', (req, res) => {
+    const body = req.body;
+    redis2.publish('products', body.mensagem).then((resposta) => {
+        console.log(resposta);
+    });
+    res.status(200);
+    res.json(body);
 });
+
+
+// http.listen(3000, () => {
+//     console.log('Listening on Port 3000');
+// });
 
 function handler (req, res) {
     fs.readFile(__dirname + '/index.html', (err, data) => {
