9f93bd5e828663f4df447835b6f55b56bd94d475
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-04-12 16:29:30 -0300

    Adds the db container for save the logs

--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const bodyParser = require('body-parser');
 const Redis = require('ioredis');
 const redisSub = new Redis(6379, 'redis');
 const redisPub = new Redis(6379, 'redis');
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'redis',
+    database: 'redis_reg'
+});
+
+/* Respostas HTTP */
+const resposta201 = {
+    'status': true,
+    'message': 'Notificação despachada com sucesso!'
+};
+
+const resposta500 = {
+    'status': false,
+    'message': 'Pode ter ocorrido um problema do serviço Redis ou do Banco de dados, tente novamente'
+};
+/* Respostas HTTP */
 
 const app = express();
 
@@ -27,11 +46,26 @@ app.get('/', (req, res) => {
 
 app.post('/publish', (req, res) => {
     const body = req.body;
-    redisPub.publish(body.token, body.mensagem).then((response) => {
-        console.log(response);
+    let redisResponse;
+    redisPub.publish(body.canal, body.mensagem).then((response) => {
+        redisResponse = response;
+        connection.query(`
+            INSERT registers (channel, message, pub_redis) VALUES ("${body.canal}", "${body.mensagem}", ${redisResponse})
+        `, (err, result, fields) => {
+            console.log(redisResponse);
+            console.log(err);
+            console.log(result);
+            console.log(fields);
+    
+            if (err || redisResponse === 0) {
+                res.status(500);
+                res.json(resposta500);
+            } else {
+                res.status(201)
+                res.json(resposta201);
+            }
+        });
     });
-    res.status(200);
-    res.json(body);
 });
 
 const server = app.listen(3000);
